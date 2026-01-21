30a3e134f1d36ce881aa5877f36b1d3e97ab82c9
pamegumach <pam.gm09@gmail.com>
2026-01-21 23:05:15 +0100

    Agrego funcionalidad a páginas de feedback

---
+++ pages/discover.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="text-center">
+            <img src="https://png.pngtree.com/png-vector/20241111/ourmid/pngtree-cat-with-laptop-vector-png-image_14336449.png" alt="Construcción" class="w-32 h-32 mx-auto mb-4" />
+            <h1 class="text-3xl font-bold mb-2">En Construcción</h1>
+            <p class="text-gray-400">Esta página está siendo desarrollada. Vuelve pronto.</p>
+        </div>
+    </div>
+</template>
